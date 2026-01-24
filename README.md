14fea2b7e2f9a175f110463b7ea6f9509e70caae
Thales M <mthalesb@proton.me>
2026-01-23 21:06:11 -0300

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+# 25_2-Polivisual
+
+**Título do TCC: POLIVISUAL: UM ESTUDO SOBRE A IMPLEMENTAÇÃO
+DA REALIDADE AUMENTADA NAS SALAS DE AULA
+BRASILEIRAS SOB UMA ÓTICA DOCENTE** 
+
+**Alunos: Thales Maia Barreto**
+
+**Semestre de Defesa: 2025-2**
+
+[PDF do TCC](./TCC_2.pdf)
+
+# TL;DR
+Para testar o Polivisual, você precisa ter o Node.js instalado em sua máquina. Caso já tenha, instale as dependências do projeto com o comando `npm install` e depois rode localmente com `npm run dev -- --host`.
+
+Utilize o endereço de rede (exibido no terminal após executar o comando para rodar o projeto localmente) no seu navegador no celular. Certifique-se de que ambos os aparelhos estejam conectados na mesma rede.
+
+# Descrição Geral
+O avanço contínuo da tecnologia tem causado profundas transformações na sociedade, in-
+cluindo o surgimento da Realidade Aumentada. No contexto educacional, essa tecnologia
+pode ser explorada de forma benéfica, especialmente nas escolas, por meio de dispositivos
+móveis. As instituições de ensino desempenham papel fundamental na formação dos jo-
+vens, e o uso adequado da tecnologia pode contribuir significativamente para a melhoria
+da qualidade do ensino. Nesse sentido, este trabalho apresenta a percepção de professores
+de diferentes regiões do Brasil sobre os desafios de implementação e os possíveis benefícios
+pedagógicas da integração da Realidade Aumentada no ensino, e propõe uma aplicação
+web utilizando tal tecnologia, denominada Polivisual, voltada ao ensino de poliedros para
+alunos do ensino fundamental por meio de smartphones. Os resultados da Avaliação Expe-
+rimental revelam um consenso entre os docentes quanto ao impacto positivo da ferramenta
+para o aluno, destacando seu potencial para promover o engajamento e tornar o ambiente
+de sala de aula mais colaborativo, consequentemente, melhorando a qualidade de ensino.
+
+# Funcionalidades
+* Escolha de poliedros: cubo, tetraedo e tronco.
+* Escolha de realce: vértice, aresta e face.
+* Visualização em 3D em Realidade Aumentada das suas escolhas.
+
+# Arquitetura
+<!-- Descreva nessa seção a arquitetura do seu código. Sugestão: use mermaid para inclusão de diagramas que ajudem a entender seu código (https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/creating-… -->
+
+```mermaid
+useCaseDiagram
+    actor "Usuário" as user
+
+    usecase "Escolher poliedro" as UC1
+    usecase "Escolher realce" as UC2
+    usecase "Começar RA" as UC3
+    usecase "Mostrar na tela poliedro\ncom realce" as UC4
+
+    user --> UC1
+    UC1 ..> UC2 : <<include>>
+    UC2 ..> UC3 : <<include>>
+    UC3 <.. UC4 : <<extend>>
+```
+
+# Dependências
+* Node.js
+
+# Execução
+* Instale as dependências do projeto: `npm install`
+* Rode localmente: `npm run dev -- --host`
---
+++ main.js
@@ -0,0 +1,107 @@
+import "./reset.css";
+import "./style.css";
+
+const containerRa = document.getElementById("container-ra");
+const botaoComecarRa = document.getElementById("botao-comecar");
+const menu = document.getElementById("menu");
+const propriedadeInput = document.getElementById("propriedade-input");
+const poliedroInput = document.getElementById("poliedro-input");
+const tooltip = document.getElementById("tooltip");
+const tooltipText = document.getElementById("tooltip-texto");
+const avisoCelular = document.getElementById("aviso-celular");
+
+const botaoVoltar = () => {
+  document.getElementById("arjs-video").remove();
+  
+  menu.style.display = "flex";
+  containerRa.innerHTML = "";
+  containerRa.style.display = "none";
+};
+
+const propriedadeTexto = (propriedade) => {
+  if (propriedade === "aresta") {
+    return "<p>Aresta é a borda de uma face de um poliedro. São as <span id='realce-propriedade-texto'>partes em azul</span> no objeto abaixo.</p>";
+  } else if (propriedade === "vertice") {
+    return "<p>Vértice é o ponto de ligação das arestas de um poliedro. São as <span id='realce-propriedade-texto'>partes em azul</span> no objeto abaixo.</p>";
+  } else if (propriedade === "face") {
+    return "<p>Face é o lado de um poliedro. São as <span id='realce-propriedade-texto'>partes em azul</span> no objeto abaixo.</p>";
+  }
+};
+
+const propriedadeObjeto = (propriedade, poliedro) => {
+  const objetos3d = {
+    "aresta-cubo": "./assets/models/cubo-aresta.glb",
+    "vertice-cubo": "./assets/models/cubo-vertice.glb",
+    "face-cubo": "./assets/models/cubo-face.glb",
+    "aresta-tetraedro": "./assets/models/tetraedro-aresta.glb",
+    "vertice-tetraedro": "./assets/models/tetraedro-vertice.glb",
+    "face-tetraedro": "./assets/models/tetraedro-face.glb",
+    "aresta-tronco": "./assets/models/tronco-aresta.glb",
+    "vertice-tronco": "./assets/models/tronco-vertice.glb",
+    "face-tronco": "./assets/models/tronco-face.glb",
+  };
+
+  return objetos3d[`${propriedade}-${poliedro}`];
+};
+
+const iniciar = () => {
+  if (!propriedadeInput.value || !poliedroInput.value) {
+    alert("Você deve escolher uma propriedade e um poliedro para começar!");
+    return;
+  }
+
+  menu.style.display = "none";
+  containerRa.style.display = "block";
+  
+  console.log("TESTE", propriedadeObjeto(
+    propriedadeInput.value,
+    poliedroInput.value
+  ))          
+
+  containerRa.innerHTML = `
+    <button id="botao-voltar">Voltar</button>
+    <div id="container-info">
+      <p>${propriedadeTexto(propriedadeInput.value)}</p>
+    </div>
+    <a-scene embedded arjs="trackingMethod: best; debugUIEnabled: false" vr-mode-ui="enabled: false;">
+      <a-marker preset="hiro">
+        <a-entity
+          position="0.10 0 0"
+          scale="0.80 0.80 0.80"
+          gltf-model="${propriedadeObjeto(
+            propriedadeInput.value,
+            poliedroInput.value
+          )}"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+    </a-scene>
+  `;
+
+  document
+    .getElementById("botao-voltar")
+    .addEventListener("click", botaoVoltar);
+};
+
+botaoComecarRa.addEventListener("click", () => {
+  iniciar();
+});
+
+tooltip.addEventListener("click", () => {
+  if (tooltipText.style.display === "none") {
+    tooltipText.style.display = "block";
+  } else {
+    tooltipText.style.display = "none";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (!isMobile) {
+    avisoCelular.style.display = "block";
+    botaoComecarRa.disabled = true;
+    botaoComecarRa.style.cursor = "not-allowed";
+    botaoComecarRa.style.backgroundColor = "gray";
+  }
+});
